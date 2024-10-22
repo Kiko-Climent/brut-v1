1b0dcdc2ee531a9badf993b9846fa8b35c30720b
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-10-22 20:10:20 +0200

    Finish About Section first approach

--- src/components/GallerySectionTwo.jsx
+++ src/components/GallerySectionTwo.jsx
@@ -1,11 +1,7 @@
-import galleryimage3 from '../assets/images/goran-petkovic.jpg'
-
 const GallerySectionOneTwo = () => {
   return (
     <section className='py-2 px-2'>
-      <div className='h-screen overflow-hidden'>
-        <img 
-        src={galleryimage3}/>
+      <div className='h-screen overflow-hidden parallax'>
       </div>
     </section>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="fixed z-10 w-full flex items-center justify-between py-1 px-2 font-regular text-white">
-      <div className="text-lg">brut *</div>
+    <nav className="fixed z-10 w-full flex items-center justify-between py-1 px-2 font-regular text-lg text-white">
+      <div className="">brut *</div>
       <ul className="flex items-center gap-1 text-base">
         <li><Link to="/">home /</Link></li>
         <li><Link to="/gallery">gallery /</Link></li>
