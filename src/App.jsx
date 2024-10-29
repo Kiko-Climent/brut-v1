9181ef554bb5fba3189c9c1a1e58f2638ea1e896
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-10-30 00:25:22 +0100

    Implement framer motion, animated routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { useState,useEffect } from "react";
-// import SplashScreen from "./components/Splash";
 import SplashScreen from "./components/Splash2";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Gallery from "./pages/Gallery"
-import About from "./pages/About"
+import AnimatedRoutes from "./components/routes/AnimatedRoutes";
 
 function App() {
   const [showSplash, setShowSplash] = useState(true); // Estado para controlar la visibilidad del SplashScreen
 
   useEffect(() => {
-    const timer = setTimeout(() => setShowSplash(false), 9000); // Mostrar SplashScreen por 3 segundos
+    const timer = setTimeout(() => setShowSplash(false), 8800); // Mostrar SplashScreen por 3 segundos
     return () => clearTimeout(timer); // Limpiar el temporizador al desmontar
   }, []);
 
@@ -22,12 +18,7 @@ function App() {
   return (
     <div className="overflow-x-hidden">
       <Router>
-        <Routes>
-          <Route path="/" element={<Layout><Home /></Layout>} />
-          <Route path="/gallery" element={<Layout><Gallery /></Layout>} />
-          <Route path="/about" element={<Layout><About /></Layout>} />
-          {/* <Route path="/splash" element={<Layout><SplashScreen /></Layout>} /> */}
-        </Routes>
+        <AnimatedRoutes />
       </Router>
     </div>
   )
