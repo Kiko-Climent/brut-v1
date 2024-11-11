82e49b5f0121901decd18a4cb32a2f20d7dbb3d9
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2024-11-11 20:18:16 +0100

    Fixing margins in live version

--- src/components/GallerySectionNine.jsx
+++ src/components/GallerySectionNine.jsx
@@ -1,8 +1,8 @@
 const GallerySectionNine = () => {
   return (
-    <section className='px-2 pt-0'>
+    <section className='px-2 pt-0 w-screen'>
       <div 
-        className='w-screen h-auto overflow-hidden parallax3' 
+        className='w-full h-auto overflow-hidden parallax3' 
       >
       </div>
     </section>
